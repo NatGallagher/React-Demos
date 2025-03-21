66eeb64f6793a1e175201ca9cf10ec1cf1435ea2
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-03-21 15:24:28 -0400

    app.js updated

--- react-demo-app-v3/src/App.js
+++ react-demo-app-v3/src/App.js
@@ -1,37 +1,59 @@
+import { useRef } from "react" 
+
 import Header from "./Components/Header"
 import Footer from "./Components/Footer"
 
 function App() {
 
-  const linkDemo1 = () => {
-    console.log("linkDemo1");
-  }
-  const linkDemo2 = () => {
-    console.log("linkDemo2");
-  }
-  const btnDemo1 = () => {
-    console.log("btnDemo1");
-  }
-
-  //components
-
- 
-
+  const divdisplay = useRef("") //refence, select manipluate jsx element ie: DOM
   
+  //functions 
+  function lnkdemo1()
+  {
+    let msgText = "# App:: lnkdemo1"
+    console.log(msgText)
+      
+    //const divdisplay = document.getElementById("divdisplay")
+    //validate if divdisplay is found
+    //divdisplay.innerText = ...
+    //react will create/generate the DOM document element code 
+
+    divdisplay.current.innerText = msgText
+
+  }
+
+  //es5+ arrow functions - optimized functions 
+  const lnkdemo2 = () =>
+  {
+    let msgText = "# App:: lnkdemo2"
+    console.log(msgText)
+    divdisplay.current.innerText = msgText
+
+  }
+
+  const btndemo1 = () =>
+  {
+    let msgText = "# App:: btndemo1"
+    console.log(msgText)
+    divdisplay.current.innerText = msgText
+
+  }
+
   return (
     <>
-    <div>
-      <Header title="React Demo App v3"/>
-      <p></p>
-      <a href="#" onClick={linkDemo1}>link Demo 1</a>
-      <p></p>
-      <a href="#" onClick={linkDemo2}>link Demo 2</a>
-      <p></p>
-      <button onClick={btnDemo1}>Button Demo 1</button>
-      <p>
-        <Footer title="Footer" year={2025}/>
-      </p>
-    </div>
+      <div>
+          <Header title="React Demo app v02.0.2"/>
+          <p></p>
+          <a href="#" onClick={lnkdemo1}>link demo 1</a>
+          <p></p>
+          <a href="#" onClick={lnkdemo2}>link demo 2</a>
+          <p></p>
+          <button onClick={btndemo1}>button demo 1</button>
+          <p></p>
+          <Footer title="footer" year={2025}></Footer>
+      </div>
+      <div id="divdisplay" ref={divdisplay}>
+      </div>
     </>
   );
 }
