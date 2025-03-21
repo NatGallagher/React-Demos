f303df0610451c6c4d135ca4422a4bc7f1118756
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-03-21 17:54:26 -0400

    errors corrected

--- react-demo-app-v3/src/App.js
+++ react-demo-app-v3/src/App.js
@@ -1,4 +1,4 @@
-import { useRef } from "react"
+import { useRef,useState } from "react"
 
 import Header from "./Components/Header"
 import Footer from "./Components/Footer"
@@ -6,6 +6,10 @@ import Footer from "./Components/Footer"
 function App() {
 
 //-- useState = creat in-memory data accessable by all fnctions/components on the current component/page
+// imported components does not have access to useState
+// useState [state data variable, function-name-to-update-state-date]
+//[destructoring] = creat array variables from a function
+
 const [strMsg, setStrMsg] = useState("")
 const divDisplay = useRef("") //reference, select manipulate jsx element ie: DOM
 
@@ -14,7 +18,10 @@ const linkDemo1 = () => {
     let msgText = "linkDemo1"
     console.log(msgText);
     divDisplay.current.innerText = msgText;
-
+    //update state varible using update state funciton
+    setStrMsg(msgText)
+    //-- error - can not update useState variable directly 
+    //strMsg = msgText
   }
 
  
@@ -28,13 +35,14 @@ const linkDemo1 = () => {
     let msgText = "linkDemo2"
     console.log(msgText);
     divDisplay.current.innerText = msgText;
+    setStrMsg(msgText)
 
   }
   const btnDemo1 = () => {
     let msgText = "btnDemo1"
     console.log(msgText);
     divDisplay.current.innerText = msgText;
-
+    setStrMsg(msgText)
   }
 
   //components
@@ -47,14 +55,15 @@ const linkDemo1 = () => {
     <div>
       <Header title="React Demo App v3"/>
       <p></p>
+      {strMsg};
+      <p></p>
       <a href="#" onClick={linkDemo1}>link Demo 1</a>
       <p></p>
       <a href="#" onClick={linkDemo2}>link Demo 2</a>
       <p></p>
       <button onClick={btnDemo1}>Button Demo 1</button>
-      <p>
+      <p></p>
         <Footer title="Footer" year={2025}/>
-      </p>
     </div>
     <div id="div-display" ref={divDisplay}>
       </div>
