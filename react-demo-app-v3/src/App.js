c5a3b69ed8803a4ad826b8ac69e9a5914fda8690
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-03-21 12:49:05 -0400

    functions added

--- react-demo-app-v3/src/App.js
+++ react-demo-app-v3/src/App.js
@@ -1,14 +1,24 @@
 function App() {
+
+  const linkDemo1 = () => {
+    console.log("linkDemo1");
+  }
+  const linkDemo2 = () => {
+    console.log("linkDemo2");
+  }
+  const btnDemo1 = () => {
+    console.log("btnDemo1");
+  }
   return (
     <>
     <div>
       <h1>Demo react Demo app v3</h1>
       <p></p>
-      <a href="#">link Demo 1</a>
+      <a href="#" onClick={linkDemo1}>link Demo 1</a>
       <p></p>
-      <a href="#">link Demo 2</a>
+      <a href="#" onClick={linkDemo2}>link Demo 2</a>
       <p></p>
-      <button>Button Demo 1</button>
+      <button onClick={btnDemo1}>Button Demo 1</button>
       <p>
         Footer @ 2025
       </p>
