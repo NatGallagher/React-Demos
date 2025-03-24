132af043b94d6042268125ad5850e4b58d3e0b13
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-03-24 15:15:56 -0400

    Header and Footer added

--- react-demo-app-v4/src/App.js
+++ react-demo-app-v4/src/App.js
@@ -1,7 +1,19 @@
 function App() {
+
+  function Header() {
+    return(
+      <h2>Demo react app v1.1</h2>
+    )
+  }
+
+  const Footer = () => {
+    return(
+      <p>Footer @ 2025</p>
+    )
+  }
   return (
     <>
-          <p>Demo react app v1.0</p>
+          <Header/>
           <div>
             <a href="#">useRef demo 1</a><p></p>
             <a href="#">useState demo 1</a><p></p>
@@ -9,7 +21,7 @@ function App() {
           <div>
             [display info]
           </div>
-          <p>Footer @ 2025</p>
+          <Footer/>
     </>
   );
 }
