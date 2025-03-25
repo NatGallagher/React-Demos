b9aa1d8f4e8a91407f23e5ad6213c08793e7ea74
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-03-25 11:53:26 -0400

    functions added and linked

--- react-demo-app-v5/src/App.js
+++ react-demo-app-v5/src/App.js
@@ -3,19 +3,30 @@ import Footer from "./components/Footer"
 
 function App() {
 
+  function lnkuseRefDemo1() {
+    let msgText = "lnkuseRefDemo1";
+    console.log(msgText);
+  }
+
+  function lnkuseStateDemo1() {
+    let msgText = "lnkuseStateDemo1"
+    console.log(msgText);
+  }
 
   
   return (
     <>
       <Header/>
           <div>
-            <a href="#">useRef demo 1</a>
+            <a href="#" onClick={lnkuseRefDemo1}>useRef demo 1</a>
             <p></p>
-            <a href="#">useState demo 1</a>
+            <a href="#">useRef demo 2</a>
+            <p></p>
+            <a href="#" onClick={lnkuseStateDemo1}>useState demo 1</a>
             <p></p>
           </div>
           <div>
-            [displa info]
+            [display info]
           </div>
       <Footer/>
     </>
