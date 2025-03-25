8c449a14d7a9c35ccf0a04b395cd168738405164
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-03-25 12:36:22 -0400

    v4 added

--- react-demo-app-v5/src/App.js
+++ react-demo-app-v5/src/App.js
@@ -1,34 +1,13 @@
-import Header from "./components/Header"
-import Footer from "./components/Footer"
+import Home from "./components/Home"
 
 function App() {
 
-  function lnkuseRefDemo1() {
-    let msgText = "lnkuseRefDemo1";
-    console.log(msgText);
-  }
-
-  function lnkuseStateDemo1() {
-    let msgText = "lnkuseStateDemo1"
-    console.log(msgText);
-  }
+  
 
   
   return (
     <>
-      <Header/>
-          <div>
-            <a href="#" onClick={lnkuseRefDemo1}>useRef demo 1</a>
-            <p></p>
-            <a href="#">useRef demo 2</a>
-            <p></p>
-            <a href="#" onClick={lnkuseStateDemo1}>useState demo 1</a>
-            <p></p>
-          </div>
-          <div>
-            [display info]
-          </div>
-      <Footer/>
+      <Home/>
     </>
   );
 }
---
+++ react-demo-app-v5/src/components/Home.js
@@ -0,0 +1,33 @@
+function Home() {
+
+    function lnkuseRefDemo1() {
+      let msgText = "lnkuseRefDemo1";
+      console.log(msgText);
+    }
+  
+    function lnkuseStateDemo1() {
+      let msgText = "lnkuseStateDemo1"
+      console.log(msgText);
+    }
+  
+    
+    return (
+      <>
+            <div>
+              <a href="#" onClick={lnkuseRefDemo1}>useRef demo 1</a>
+              <p></p>
+              <a href="#">useRef demo 2</a>
+              <p></p>
+              <a href="#" onClick={lnkuseStateDemo1}>useState demo 1</a>
+              <p></p>
+            </div>
+            <div>
+              [display info 1]
+            </div>
+        
+      </>
+    );
+  }
+  
+  export default Home;
+  
