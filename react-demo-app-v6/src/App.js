605918d733b7ab0086e2455c1ce479de35cb927b
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-03-25 18:22:08 -0400

    v6 import Hash with Routes

--- react-demo-app-v6/src/App.js
+++ react-demo-app-v6/src/App.js
@@ -1,10 +1,17 @@
+import {HashRouter, Routes, Route} from "react-router-dom"
+import About from "./components/About"
 import Home from "./components/Home"
 
 function App() {
 
   return (
    <>
-    <Home/>
+    <HashRouter>         
+      <Routes>      
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
+    </HashRouter>    
    </>
   );
 }
