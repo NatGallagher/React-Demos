c2e10ebd08494f6a3cb1c8a2a222e789016b9050
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-03-25 18:12:18 -0400

    v6 Home.js created and imported

--- react-demo-app-v6/src/App.js
+++ react-demo-app-v6/src/App.js
@@ -1,25 +1,10 @@
+import Home from "./components/Home"
 
 function App() {
 
-  function useRefDemo1() {
-    let msgText = "useRefDemo1"
-    console.log(msgText);
-  }
-
-  function useStateDemo1() {
-    let msgText = "useStateDemo1";
-    console.log(msgText);
-  }
-
   return (
    <>
-    <a href="#" onClick={useRefDemo1}>useRefDemo1</a>
-    <p></p>
-    <a href="#" onClick={useStateDemo1}>useStateDemo1</a>
-    <p></p>
-    <div>
-      [display info]
-    </div>
+    <Home/>
    </>
   );
 }
---
+++ react-demo-app-v6/src/components/Home.js
@@ -0,0 +1,27 @@
+function Home() {
+
+    function useRefDemo1() {
+      let msgText = "useRefDemo1"
+      console.log(msgText);
+    }
+  
+    function useStateDemo1() {
+      let msgText = "useStateDemo1";
+      console.log(msgText);
+    }
+  
+    return (
+     <>
+      <a href="#" onClick={useRefDemo1}>useRefDemo1</a>
+      <p></p>
+      <a href="#" onClick={useStateDemo1}>useStateDemo1</a>
+      <p></p>
+      <div>
+        [display info 1]
+      </div>
+     </>
+    );
+  }
+  
+  export default Home;
+  
