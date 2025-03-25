756f1cfe219d470fe15474474af00374ef153156
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-03-25 13:50:00 -0400

    about page added

--- react-demo-app-v5/src/App.js
+++ react-demo-app-v5/src/App.js
@@ -1,4 +1,5 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom"
+import About from "./components/About"
 import Home2 from "./components/Home2"
 import Header from "./components/Header";
 import Footer from "./components/Footer"
@@ -14,6 +15,7 @@ function App() {
       <BrowserRouter>            
       <Routes>      
         <Route path="/" element={<Home2/>}/>
+        <Route path="/about" element={<About/>}/>
       </Routes>
     </BrowserRouter>    
 
