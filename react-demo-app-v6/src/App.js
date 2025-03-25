c919afa3f56283a6c5c60839bd2819e72a16cd36
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-03-25 18:05:48 -0400

    v6 functions added and linked

--- react-demo-app-v6/src/App.js
+++ react-demo-app-v6/src/App.js
@@ -1,10 +1,21 @@
 
 function App() {
+
+  function useRefDemo1() {
+    let msgText = "useRefDemo1"
+    console.log(msgText);
+  }
+
+  function useStateDemo1() {
+    let msgText = "useStateDemo1";
+    console.log(msgText);
+  }
+
   return (
    <>
-    <a href="#">useRefDemo1</a>
+    <a href="#" onClick={useRefDemo1}>useRefDemo1</a>
     <p></p>
-    <a href="#">useStateDemo1</a>
+    <a href="#" onClick={useStateDemo1}>useStateDemo1</a>
     <p></p>
     <div>
       [display info]
