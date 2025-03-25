8b4007434e5d6d7c8ff1cc31aca4b92f02ea6899
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-03-25 18:30:47 -0400

    v6 imported Header and NavBar

--- react-demo-app-v6/src/App.js
+++ react-demo-app-v6/src/App.js
@@ -1,12 +1,16 @@
 import {HashRouter, Routes, Route} from "react-router-dom"
 import About from "./components/About"
 import Home from "./components/Home"
+import NavBar from "./components/NavBar"
+import Header from "./components/Header"
 
 function App() {
 
   return (
    <>
-    <HashRouter>         
+    <HashRouter> 
+      <NavBar/>
+      <Header/>        
       <Routes>      
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
