2c3b4f4c2a317307506547879fb7339e71f13e8d
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-03-26 12:24:30 -0400

    v7 HashRouter imported

--- react-demo-app-v7/src/App.js
+++ react-demo-app-v7/src/App.js
@@ -1,10 +1,15 @@
+import {HashRouter, Routes, Route} from "react-router-dom"
 import Home from "./components/Home"
 
 function App() {
 
   return (
     <>
-      <Home/>      
+      <HashRouter>        
+      <Routes>      
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </HashRouter>         
     </>
   );
 }
