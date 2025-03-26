606908dac648fb3ea907792f94a786a71a0ff14e
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-03-26 12:30:26 -0400

    v7 NavBar added

--- react-demo-app-v7/src/App.js
+++ react-demo-app-v7/src/App.js
@@ -1,12 +1,14 @@
 import {HashRouter, Routes, Route} from "react-router-dom"
 import Home from "./components/Home"
 import About from "./components/About"
+import NavBar from "./components/NavBar"
 
 function App() {
 
   return (
     <>
-      <HashRouter>        
+      <HashRouter>
+        <NavBar/>        
       <Routes>      
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
