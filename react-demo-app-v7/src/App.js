90ba69826ab6b2c4fa2d935300ccdc5dc0c26435
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-03-26 12:14:27 -0400

    v7 app.js updated

--- react-demo-app-v7/src/App.js
+++ react-demo-app-v7/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>Live Update ...1</p>
-      </header>
-    </div>
+    <>
+      <h2>React Demo 3.0</h2>
+      <a href="#">useRefDemo1</a>
+      <p></p>
+      <a href="#">useStateDemo1</a>
+      <p></p>
+      <div>
+        [display info 1.0]
+      </div>
+    </>
   );
 }
 
